f9572f69ca6f222bc27e980a5c1a35f2d12d9836
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-09-16 15:41:47 +0300

    Skynet birth

---
+++ rcgnize.py
@@ -0,0 +1,26 @@
+import speech_recognition as sr
+recognizer = sr.Recognizer()
+microphone = sr.Microphone()
+
+
+def recognize_user(*args: tuple):
+    with microphone:
+        data = ""
+
+        recognizer.adjust_for_ambient_noise(microphone, duration=0.5)
+
+        try:
+            print('speak: ')
+            audio = recognizer.listen(microphone, 5, 5)
+
+        except sr.WaitTimeoutError:
+            print('Check your micro and speak')
+            return
+        try:
+            print('Recognizing... ')
+            data = recognizer.recognize_google(audio, language='ru').lower()
+
+        except sr.UnknownValueError:
+            print('Check your internet connection')
+
+        return data
