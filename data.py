123ceff6e92d1711e4000d1aefc92728febf62a2
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-09-16 15:43:03 +0300

    Update data.py

--- data.py
+++ data.py
@@ -3,4 +3,6 @@
 # If u followed instructions from there and get SNlM0e value not found. Double-check __Secure-1PSID value or pass it as token='xxxxx'.
 # Check it out: https://github.com/dsdanielpark/Bard-API/issues/99
 
-TOKEN = 'bAg-irIA7AmVTYTHSuQ4iDV6RnAhTjeWAtZtSWB7Fua_BqKzmHJqmPNUbOVleDoM3RuXwA.'
+TOKEN = 'XXXX.'
+
+# And yeah. Replace XXXX. with your token (coockie - __Secure-1PSID)
