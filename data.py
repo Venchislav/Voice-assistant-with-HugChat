bd19c200ad3699a873d1c8705d7dc1ceee140cdc
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-09-16 15:41:20 +0300

    woah

---
+++ data.py
@@ -0,0 +1,6 @@
+# I used unofficial BardAPI
+# link to GitHub: https://github.com/dsdanielpark/Bard-API/tree/main
+# If u followed instructions from there and get SNlM0e value not found. Double-check __Secure-1PSID value or pass it as token='xxxxx'.
+# Check it out: https://github.com/dsdanielpark/Bard-API/issues/99
+
+TOKEN = 'bAg-irIA7AmVTYTHSuQ4iDV6RnAhTjeWAtZtSWB7Fua_BqKzmHJqmPNUbOVleDoM3RuXwA.'
