35c34759ee86b517ad1391ee9e0b4f37511f5733
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-09-17 11:26:19 +0300

    libraries used

---
+++ libs_pack.py
@@ -0,0 +1,8 @@
+import speech_recognition as sr
+from bardapi import Bard
+from rcgnize import recognize_user
+import subprocess
+from data import TOKEN
+from termcolor import colored
+import webbrowser as wb
+from _tts import say
