249a3af1c47e5dc130a1269de9683a64bc61c071
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-09-17 12:10:45 +0300

    file to work with files and programs on pc

---
+++ pc_controls.py
@@ -0,0 +1,10 @@
+import os
+
+programs = {'chrome': 'C:\Program Files\Google\Chrome\Application\chrome.exe',
+            'пайчарм': 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\PyCharm Community Edition 2023.1.2.lnk',
+            'telegram': r'C:\Users\rayga\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Telegram Desktop\telegram.lnk',
+            'steam': r'C:\Users\rayga\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Steam.lnk',
+            'проводник': ''}
+
+def open_(program):
+    os.startfile(programs[program])
