c68cc578c50f84636a43b4f66eaaa8d2b1241853
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-09-17 12:10:14 +0300

    main file of assistant

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
         input_ = recognize_user()
         # Here I mean if input is bye program breaks
         if input_ == 'пока':
+            say(random.choice(['Пока', 'До скорых встреч', 'Удачи', 'Обращайтесь ещё!']))
             break
         if (input_ == ' ') or (input_ == ''):
             print('Не молчите! \n{"-"*60}')
@@ -24,6 +25,10 @@ if __name__ == '__main__':
 
         if ('найди в браузере' in input_) or ('найди в интернете' in input_):
             wb.open(f'https://www.google.com/search?q={input_.replace("найди в браузере", "").replace("найди в интернете", "")}')
+
+        if input_.startswith('открой'):
+            print(input_ + f'\n{"-"*60}')
+            open_(' '.join(input_.split()[1:]))
         else:
             print(colored(f'User (you) - {input_}', "red"))
             print(f'{"-"*60}\n connecting Bard: \n {"-"*60}')
