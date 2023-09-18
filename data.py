42c04c9bd7254ff1cd100e1304a70cf085c13f57
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-09-18 17:12:34 +0300

    Add files via upload

--- data.py
+++ data.py
@@ -3,6 +3,5 @@
 # If u followed instructions from there and get SNlM0e value not found. Double-check __Secure-1PSID value or pass it as token='xxxxx'.
 # Check it out: https://github.com/dsdanielpark/Bard-API/issues/99
 
-TOKEN = 'XXXX.'
+TOKEN = 'bAg-igSCxj3zKwRJUEWsHWvrvj3lmz5y8oz_n2BXbq8vhVGHDcqDwUKzt7NbwCYoYljgRQ.'
 
-# And yeah. Replace XXXX. with your token (coockie - __Secure-1PSID)
