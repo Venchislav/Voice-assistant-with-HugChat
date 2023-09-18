f203fb8c37cd2adb90e3ed8463dad1b82caafe53
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-09-18 17:12:44 +0300

    Add files via upload

--- libs_pack.py
+++ libs_pack.py
@@ -1,10 +1,24 @@
 import speech_recognition as sr
-from bardapi import Bard
 from rcgnize import recognize_user
 import subprocess
 from data import TOKEN
 from termcolor import colored
+from hugchat import hugchat
+from hugchat.login import Login
+from config import email, passwd
 import webbrowser as wb
 from _tts import say
 from pc_controls import open_
 import random
+from translate import Translator
+translator = Translator(to_lang="ru")
+
+sign = Login(email, passwd)
+cookies = sign.login()
+
+
+cookie_path_dir = "./cookies_snapshot"
+sign.saveCookiesToDir(cookie_path_dir)
+
+
+chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
